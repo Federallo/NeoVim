950d4cc26c577d875d67ebc49af900fe88fd2722
Federallo <aligiassioma72@gmail.com>
2024-02-20 20:10:46 +0100

    adjusted alpha menu

--- .config/nvim/lua/plugins-configs/alpha.lua
+++ .config/nvim/lua/plugins-configs/alpha.lua
@@ -14,9 +14,9 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
 	dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
-	dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
-    	dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-	dashboard.button( "l", "Lazy", ":Lazy<CR>"),
+	dashboard.button( "f", "  Find file", ":Telescope find_files<CR>"),
+    	dashboard.button( "r", "  Recent"   , ":Telescope oldfiles<CR>"),
+	dashboard.button( "l", "󰒲  Lazy", ":Lazy<CR>"),
         dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
 }
 
